e631317041d7d37fd0c775ea20ec9742ed1b1521
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-05-09 16:25:11 -0600

    added inputs

--- Develop/script.js
+++ Develop/script.js
@@ -3,9 +3,34 @@ var lowerCase = false;
 var upperCase = false;
 var specialChar = false;
 var includeNum = false;
+var length = 0;
 
 function generatePassword(){
+  length = window.prompt("Please enter the length of your password which can be between 8 - 128 characters long.");
+  length = Number(length);
+  if (length < 8 || length > 128) {
+    window.alert("ERROR: Please enter a password length value between 8 and 128 characters long.");
+  }
+  else {
+    lowerCase = window.confirm("Do you want the password to contain lower case letters?");
+    upperCase = window.confirm("Do you want the password to contain upper case letters?");
+    specialChar = window.confirm("Do you want the password to contain special characters?");
+    includeNum = window.confirm("Do you want the password to contain numbers?");
+    if ((lowerCase === false && upperCase === false) && (specialChar === false && includeNum === false) ){
+      window.alert("ERROR: You must pick at least one type of character to include in the password!");
+    }
+    else {
+      return passwordCreator();
+    }
+  }
+}
+
+function passwordCreator() {
   
+}
+
+function randomBettween() {
+  return Math.floor(Math.random() * min + max);
 }
 
 // Get references to the #generate element
